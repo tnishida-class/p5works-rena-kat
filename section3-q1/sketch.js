54d331a895a325e821177b7dc8b0c7b96da14e0d
rena-kat <72774887+rena-kat@users.noreply.github.com>
2020-11-17 11:43:25 +0900

    homework3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,23 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon(0,0,0,"I love keyakizaka46",50,50);
 }
 
-function balloon(t){
-  let w = textWidth(t);
-  let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+function balloon(a,b,c,t,x,y){
+ let w = textWidth(t);
+ let h = textAscent() + textDescent();
+ let p = 2;
+
+noStroke();
+ fill(a,b,c);
+rect(x,y,w+p*2,h+p*2);
+  beginShape();
+   vertex(w+p*2+x,(h+p*2)*2/3+y);
+   vertex(w+p*2+x,(h+p*2)/3+y);
+   vertex(w+p*2+x+20,y+(h+p*2)/2);
+
+  endShape();
+ fill(255)
+ text(t,x+p,y+h+p);
 }
