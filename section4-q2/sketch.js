78374eb7ddb73405b85be9de701b7181fd49da88
rena-kat <72774887+rena-kat@users.noreply.github.com>
2020-11-24 06:56:24 +0900

    homework4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,5 +1,5 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy,angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -7,11 +7,18 @@ function setup(){
   y = height / 2;
   vx = 8;
   vy = 8;
+  angle=0
 }
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+noStroke();
+fill(255,255,255)
+star(x,y,10,angle);
+x+=vx
+y-=vy
+angle+=20;
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -24,7 +31,9 @@ function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+theta+=angle
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
